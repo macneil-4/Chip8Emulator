23feeac0e960d5ed2e4394e6996be14ca2e35973
Ben S <sheebe12@gmail.com>
2019-09-22 20:36:39 +1000

    move around some comments planning for program structure

--- chip8.c
+++ chip8.c
@@ -238,11 +238,4 @@ void emulate_cycle(struct VirtualMachine* vm) {
         // Execute Opcode
 
   // Update timers
-
-    // if draw flag set, update the screen Because the system does not draw every cycle, we should set a draw flag when we need to update our screen. Only two opcodes should set this flag:
-    // 0x00E0 – Clears the screen
-    // 0xDXYN – Draws a sprite on the screen
-
-
-    // store key press state (press and release) - If we press or release a key, we should store this state in the part that emulates the keypad-
 }
--- main.c
+++ main.c
@@ -26,6 +26,17 @@ int main()
     printf("Now starting emulation loop...\n");
     while (1) {
         emulate_cycle(&vm);
+
+        // draw_screen (if required)
+
+        // if draw flag set, update the screen Because the system does not draw every cycle, we should set a draw flag when we need to update our screen. Only two opcodes should set this flag:
+        // 0x00E0 – Clears the screen
+        // 0xDXYN – Draws a sprite on the screen
+
+
+        // handle_keypress (if required)
+
+        // store key press state (press and release) - If we press or release a key, we should store this state in the part that emulates the keypad-
     }
     return 0;
 }
