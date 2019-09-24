0586a4f4b5739fe04dd0ace8a913e001b2360b5f
Ben S <sheebe12@gmail.com>
2019-09-24 20:40:38 +1000

    setting up SDL and init window

--- Chip8Emulator.pro
+++ Chip8Emulator.pro
@@ -4,11 +4,15 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.c \
-    chip8.c
+    chip8.c \
+    display.c
 
 HEADERS += \
-    chip8.h
+    chip8.h \
+    display.h
 
 DISTFILES += \
     test.txt \
     opcodes.txt
+
+LIBS += -lSDL2main -lSDL2
--- main.c
+++ main.c
@@ -1,11 +1,41 @@
 #include "chip8.h"
-// include SDL graphics and input
-#include "chip8.h"
 // "chip8.h" cpu core implementation
+#include <SDL2/SDL.h>
+// include SDL graphics and input
 
-int main()
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
     // setup graphics Setup the graphics (window size, display mode, etc) and input system (bind callbacks)
+
+    // setting up SDL
+    SDL_Window* window = NULL;
+
+    SDL_Surface* screenSurface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            SDL_UpdateWindowSurface(window);
+
+            SDL_Delay(2000);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     // setup input
 
@@ -26,6 +56,8 @@ int main()
 
     // emulation loop
     printf("Now starting emulation loop...\n");
+    printf("Skipping emulation loop"); // temporary
+    return 0;
     while (1) {
         emulate_cycle(&vm);
 
@@ -40,5 +72,6 @@ int main()
 
         // store key press state (press and release) - If we press or release a key, we should store this state in the part that emulates the keypad-
     }
+
     return 0;
 }
